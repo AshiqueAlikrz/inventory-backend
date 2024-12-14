ea38df10ea4b0ff10ababd70c4cd5ad040fb6705
AshiqueAlikrz <ashiquealikrz9@gmail.com>
2024-12-14 12:22:11 +0400

    f

--- index.ts
+++ index.ts
@@ -6,26 +6,32 @@ import cors from "cors";
 const app: Express = express();
 
 // MongoDB Connection
-const mongoUri = "mongodb+srv://ashiquealikrz9:WKssz08YogiTf7n2@cluster0.y2rdr.mongodb.net/alwainvo?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(mongoUri);
+const mongoUri = process.env.MONGO_URI || "your_mongo_uri_here";
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log(`Connected to MongoDB at ${mongoUri}`);
-});
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
 
-// WKssz08YogiTf7n2
+connectDB();
 
-app.get("/", (req, res) => {
-  res.send("  hello working");
-});
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, working!");
+});
+
 app.use("/api/reports", Report);
 
-app.listen(8081, () => {
-  console.log("Listening on port 8081...");
+// Start the server
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
