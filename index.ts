dbd33afcc209ca815956dcf91a331967af7ef48e
AshiqueAlikrz <ashiquealikrz9@gmail.com>
2024-12-14 21:27:28 +0400

    f

--- index.ts
+++ index.ts
@@ -2,23 +2,23 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import { router as Report } from "./apps/report/route";
 import cors from "cors";
+import dotenv from "dotenv";
 
 // Initialize Express app
 const app: Express = express();
+dotenv.config();
 
-// MongoDB Connection URI
-const mongoUri = "mongodb+srv://alwahdainventory:WKssz08YogiTf7n2@cluster0.y2rdr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// const mongoUri = "mongodb://localhost:27017/alwahdainventory";
+// MongoDB Connection URI from .env file
+const mongoUri = process.env.MONGODB_CONNECTION;
 
 // MongoDB Connection Function
 const connectDB = async () => {
   try {
-    // No need to specify useNewUrlParser or useUnifiedTopology in Mongoose 6+
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUri as string);
     console.log("Connected to MongoDB successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit the process if MongoDB connection fail 
+    process.exit(1); // Exit the process if MongoDB connection fails
   }
 };
 
@@ -30,9 +30,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-
 app.get("/", (req, res) => {
-  res.send("Hello, working!"); 
+  res.send("Hello, working!");
 });
 
 app.use("/api/reports", Report);
